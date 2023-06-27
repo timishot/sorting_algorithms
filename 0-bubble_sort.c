3c3f3dc82c3990434474edae07c7108a3f88a7e3
timishot <timidotcom58@gmail.com>
2023-06-27 21:03:49 +0100

    making update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,17 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
 
-	size_t i;
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		size_t j;
-		for (j = 0; j < size -1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				/*Swap array[j] and array[j + 1]*/
 				int temp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
