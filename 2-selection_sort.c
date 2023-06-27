8611113e203133df85d6373d87aa7d9718c179e3
timishot <timidotcom58@gmail.com>
2023-06-27 20:28:51 +0100

    udatated the selection sort algorthm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,43 @@
 #include "sort.h"
 
+
 /**
- * selection_sort - a sorting algorithm
- * @array: the array to be sorted
- * @size: the size of the array
+ * swap - it  is uses to swap two integers
+ * @a: first to to be swap.
+ * @b: second.
  */
 
-void swap(int *a; int *b)
+void swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
+/**
+ * selection_sort - a sorting algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
+
 	for (i = 0; i < size; i++)
 	{
 		min_idx = i;
-		for (j = i + 1; J < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
+			if (array[j] < array[min_idx])
 			{
-				min_index = j;
+				min_idx = j;
 			}
 		}
 
 		/*swap the minimum element with the first element  of the unsorted part*/
-		swap(&array[min_index, &array[i]]);
+		swap(&array[min_idx, &array[i]]);
 
 		/*print the array after each swap (optional)*/
 		for (int k = 0; k < size; k++)
