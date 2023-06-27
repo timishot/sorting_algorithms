6bbd5582515da7ab04a4f12dd542c6977ce721cc
Timilehin Adeniran <101478000+timishot@users.noreply.github.com>
2023-06-27 20:52:02 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,46 +1,47 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap - it  is uses to swap two integers
+ * @a: first to to be swap.
+ * @b: second.
  */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+void swap(int *a, int *b)
+{
+        int temp = *a;
+        *a = *b;
+        *b = temp;
 }
 
 /**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
+ * selection_sort - a sorting algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
  */
-void bubble_sort(int *array, size_t size)
+
+
+void selection_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+        size_t i, j, min_idx, k;
 
-	if (array == NULL || size < 2)
-		return;
+        for (i = 0; i < size; i++)
+        {
+                min_idx = i;
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[j] < array[min_idx])
+                        {
+                                min_idx = j;
+                        }
+                }
 
-	while (bubbly == false)
-	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_ints(array + i, array + i + 1);
-				print_array(array, size);
-				bubbly = false;
-			}
-		}
-		len--;
-	}
+                /*swap the minimum element with the first element  of the unsorted part*/
+                swap(&array[min_idx], &array[i]);
+
+                /*print the array after each swap (optional)*/
+                for (k = 0; k < size; k++)
+                        printf("%d ", array[k]);
+
+                printf("\n");
+        }
 }
